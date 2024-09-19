2b35a0a5f152a0b106b7381861d322a54c4e5bb7
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2024-09-19 07:40:42 -0300

    ffv

---
+++ Flashcards - 3o/perguntas.js
@@ -0,0 +1,117 @@
+
+criaCartao(
+'Biologia',
+'Todas as células possuem parede celular?',
+'Não, apenas as células vegetais apresentam esta estrutura.')
+
+criaCartao(
+'Biologia',
+'Qual é a pentose encontrada na molécula do DNA?',
+'A pentose encontrada nas moléculas de DNA é a desoxidoribose.')
+
+
+criaCartao(
+'Biologia',
+'Quais são as bases nitrogenadas?',
+'Adenina, Guanina, citosina, timina e Uracil.')
+
+
+criaCartao(
+'Biologia',
+'Qual é o tipo de ligação estabelecida entre as bases nitrogenadas?',
+'São realizadas ligações do tipo ponte de hidrogênio.')
+
+
+criaCartao(
+'Biologia',
+'Pelo o que os ácidos nucleicos são constituídos?',
+'São constituídos por nucleotídeos.')
+
+criaCartao(
+'Física',
+'Nas ondas longitudinais a vibração da fonte é _______ ao deslocamento da onda.',
+'Paralela.')
+
+
+criaCartao(
+'Física',
+'Como é chamado o ponto mais alto e o mais baixo de uma onda?',
+'O ponto mais alto de uma onda é denominado crista enquanto o mais baixo vale.')
+
+
+criaCartao(
+'Física',
+'O que é uma fonte de luz luminosa?',
+'É uma fonte cujo, átomos e moléculas são capazes de emitir radiações visíveis.')
+
+
+criaCartao(
+'Física',
+'Como é denominado um conjunto de raios de luz?',
+'É denominado feixe de luz.')
+
+
+criaCartao(
+'Física',
+'O que define a formação de uma sombra ou penumbra?',
+'O tipo de luz que incide e a forma em que ela incide.')
+
+
+criaCartao(
+'Química',
+'Quais são os postulados de kekulé',
+'O átomo de carbono é tetravalente, as quatro valências do carbono são iguais entre si e os os átomos de carbono se ligam formando cadeias carbônicas.')
+
+
+criaCartao(
+'Química',
+'Quais são as classificações das cadeias carbônicas?',
+'Aberta ou acíclica, fechada ou cíclica e mista.')
+
+
+criaCartao(
+'Química',
+'O que uma cadeia carbônica acíclica normal apresenta?',
+'Uma cadeia acíclica normal apresenta, átomos de carbono primários ou secundários.')
+
+
+criaCartao(
+'Química',
+'Qual é a principal característica dos alcadienos?',
+'A presença de duas duplas de ligações')
+
+
+criaCartao(
+'Química',
+'Quais são as classificações dos óxidos em função do comportamento?',
+'Óxidos básicos,óxidos ácidos,óxidos anfóteros,óxidos neutros,óxidos salinos, peróxidos,superóxidos.')
+
+
+criaCartao(
+'Matemática',
+'O que é uma progressão aritmética?',
+'É uma sequência na qual cada termo é igual a anterior somado uma constante r.')
+
+
+criaCartao(
+'Matemática',
+'Qual é a lei de formação?',
+'f(x) = log a^x.')
+
+
+criaCartao(
+'Matemática',
+'Como o seno de um ângulo agudo é calculado?',
+'Sen = cateto oposto/ hipotenusa.')
+
+
+criaCartao(
+'Matemática',
+'Como tangente de um ângulo é calculada?',
+'Tan = cateto oposto/ cateto adjacente.')
+
+
+criaCartao(
+'Matemática',
+'O que é o Teorema de Carnot?',
+'a²=b²+c²-2bc cosA')
