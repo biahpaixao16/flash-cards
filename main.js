d43b56aa7793334a299fb1f820d00579f65fd796
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2024-09-19 08:41:46 -0300

    fdbdf

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function criaCartao  (categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let Cartao = document.createElement('article');
     Cartao.className = 'cartao';
+    Cartao.innerHTML = ``
+    
 }
 
