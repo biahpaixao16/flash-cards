fb6963c7004076e548349d35597f78b94c728ed1
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2024-09-19 08:43:38 -0300

    bgb

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ function criaCartao  (categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let Cartao = document.createElement('article');
     Cartao.className = 'cartao';
-    Cartao.innerHTML = ``
+    Cartao.innerHTML = `<div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class="pergunta-cartao">
+    <p>${pergunta}</p>
+    <div class="resposta-cartao">
+        <p>${resposta}</p>
     
+
+ </div>
+</div>`
+
 }
 
