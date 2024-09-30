7fba3f5da454e267da8f470936c4c8079c604678
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2024-09-30 09:01:46 -0300

    cghjv

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ function criaCartao  (categoria,pergunta,resposta){
         <p>${resposta}</p>
     </div>
     </div>`
+    let respostaVisivel = false;
+function viraCartao(){
+ respostaVisivel = !respostaVisivel;
+ cartao.classList.toggle('active', respostaVisivel);
+}
+cartao.addEventListener('click', viraCartao);
          container.appendChild(Cartao);
 }
 
