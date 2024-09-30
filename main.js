6a1635d8b7a997b4274c133f2ba5187e128655f7
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2024-09-30 09:42:53 -0300

    fr

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-function criaCartao  (categoria,pergunta,resposta){
-    let container = document.getElementById('container');
-    let Cartao = document.createElement('article');
-    Cartao.className = 'cartao';
-    Cartao.innerHTML = `
-    <div class="conteudo-cartao">
-    <h3>${categoria}</h3>
-    <div class="pergunta-cartao">
-    <p>${pergunta}</p>
-    <div class="resposta-cartao">
-    <p>${resposta}</p>
-    </div>
-    </div>
-    `
-    let respostaVisivel = false;
-    function viraCartao(){
-    respostaVisivel = !respostaVisivel;
-    Cartao.classList.toggle('active', respostaVisivel);
+async (params) => {
+    function criaCartao  (categoria,pergunta,resposta){
+        let container = document.getElementById('container');
+        let Cartao = document.createElement('article');
+        Cartao.className = 'cartao';
+        Cartao.innerHTML = `
+        <div class="conteudo-cartao">
+        <h3>${categoria}</h3>
+        <div class="pergunta-cartao">
+        <p>${pergunta}</p>
+        <div class="resposta-cartao">
+        <p>${resposta}</p>
+        </div>
+        </div>
+        `
+        let respostaVisivel = false;
+        function viraCartao(){
+        respostaVisivel = !respostaVisivel;
+        Cartao.classList.toggle('active', respostaVisivel);
+    }
+        Cartao.addEventListener('click', viraCartao);
+    container.appendChild(Cartao);
+    }
+    
+    
 }
-    Cartao.addEventListener('click', viraCartao);
-container.appendChild(Cartao);
-}
-
